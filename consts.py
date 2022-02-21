e435e0d6312b8001e0ea3d5dbc5ff550a0545e30
mariing9 <maring@coreai.ai>
2022-02-21 16:07:13 +0200

    Added classification labels

--- consts.py
+++ consts.py
@@ -1,1 +1,17 @@
 BASE_LOG_DIR = './logs'
+
+CLASSES = ['Apple Black rot', 'Apple Cedar rust', 'Apple scab',
+           'Apple healthy', 'Blueberry healthy', 'Cherry healthy',
+           'Cherry Powdery mildew', 'Corn Cercospora Gray laef spot',
+           'Corn Common rust', 'Corn healthy', 'Corn Northern Leaf Bligh',
+           'Grape Black rot', 'Grape Esca Black Measles', 'Grape healthy',
+           'Grape Leaf blight Isariopsis Leaf Spot',
+           'Orange Haunglongbing Citrus', 'Peach Bacterial spot',
+           'Peach healthy', 'Pepper bell Bacterial spot',
+           'Pepper bell healthy', 'Potato Early blight', 'Potato healthy',
+           'Potato Late blight', 'Raspberry healthy', 'Soybean healthy',
+           'Squash Powdery mildew', 'Strawberry healthy',
+           'Strawberry Leaf scorch', 'Totamo Bacterial spot',
+           'Tomato Early blight', 'Tomato healthy', 'Tomato Late blight',
+           'Tomato Leaf Mold', 'Tomat mosaic virus', 'Tomato Target Spot',
+           'Tomato Two spotted spider mite', 'Tomato Yellow Leaf Curl Virus']
